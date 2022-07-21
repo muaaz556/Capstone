2d14afecfeef2f6e525822d09552661a09adee45
am4rahma <am4rahma@uwaterloo.ca>
2022-07-21 15:00:40 -0400

    button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,15 +8,15 @@ const HomeScreen = ({ navigation }) => {
             onPress={() =>
                 navigation.navigate('Distance')
             }
-            style={{width:170,backgroundColor:'#99004d',marginTop:20,}}>
-                    <Text>Object Sensing Screen</Text>
+            style={{width:250,height: 100, backgroundColor:'#99004d',marginTop:20, borderRadius:20}}>
+                    <Text style={{color: "#FFFFFF", fontSize:20, fontWeight:'bold'}}>Object Sensing Screen</Text>
             </Button>
             <Button
                 onPress={() =>
                     navigation.navigate('TTS')
                 }
-                style={{width:170,backgroundColor:'#99004d',marginTop:20,}}>
-                <Text>Text-to-Speech Screen</Text>
+                style={{width:250,height: 100, backgroundColor:'#99004d',marginTop:20, borderRadius:20}}>
+                <Text style={{color: "#FFFFFF", fontSize:20, fontWeight:'bold'}}>Text-to-Speech Screen</Text>
             </Button>
 
         </View>
