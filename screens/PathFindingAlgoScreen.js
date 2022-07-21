8578af04be34b9bf4776a362caeaeb6cd38155f7
Salmaan Khan <salmaan_khan_35@hotmail.com>
2022-07-21 17:25:52 -0400

    Copied Template Screen File into new Path Finding Algo Screen

---
+++ screens/PathFindingAlgoScreen.js
@@ -0,0 +1,14 @@
+
+import {Button} from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PathFindingAlgoScreen = ({ navigation }) => {
+    return (
+        <View style = {{flex:1, alignItems:"center", justifyContent:"center"}}>
+            <Text>Sample text</Text>
+        </View>
+
+    );
+  };
+
+  export default TemplateScreen;
