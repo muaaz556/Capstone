8b619e4048e129820dcf4c570995db7ceef2c0e9
Girujan Naguleswaran <ngirujan@gmail.com>
2022-09-19 23:55:04 -0400

    Reverting to new project code

--- App.js
+++ App.js
@@ -1,62 +1,112 @@
-import {SplashScreen} from 'expo';
-import * as Font from 'expo-font';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider } from 'native-base';
-import HomeScreen from './screens/HomeScreen';
-import DistanceSensingScreen from './screens/DistanceSensingScreen';
-import TTSScreen from './screens/TTSScreen';
-import PathFindingAlgoScreen from './screens/PathFindingAlgoScreen';
-import PathDiagramScreen from './screens/PathDiagramScreen';
-import LocationScreen from './screens/LocationScreen';
-import Accelerometer from './screens/AccelerometerScreen';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const Stack = createNativeStackNavigator();
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-export default function App() {
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <NativeBaseProvider>
-        <NavigationContainer theme={{colors: {background:'#e7f4fd'}}}>
-          <Stack.Navigator screenOptions={{headerShown:false}}>
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-            />
-            <Stack.Screen
-              name="Distance"
-              component={DistanceSensingScreen}
-            />
-            <Stack.Screen
-              name="TTS"
-              component={TTSScreen}
-            />
-            <Stack.Screen
-              name="PathFinding"
-              component={PathFindingAlgoScreen}
-            />
-            <Stack.Screen
-              name="PathDiagram"
-              component={PathDiagramScreen}
-            />
-            <Stack.Screen
-              name="Location"
-              component={LocationScreen}
-            />
-            <Stack.Screen
-              name="Accelerometer"
-              component={Accelerometer}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-    </NativeBaseProvider>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
-}
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e7f4fd',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
+
+export default App;
