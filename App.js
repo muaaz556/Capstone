671bb95fa0b5c964adfa11713a983372b034374e
MUAAZ MAHMUD <m7mahmud@uwaterloo.ca>
2022-10-25 19:50:03 -0400

    Added in kalman filter

--- App.js
+++ App.js
@@ -1,17 +1,109 @@
 import React from 'react';
 
-import {
-  Text,
-  View,
-} from 'react-native';
+// import {
+//   Text,
+//   View,
+// } from 'react-native';
+
+import { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { Button, View, Text } from 'react-native';
+import GetLocation from "react-native-get-location";
+import KalmanFilter from 'kalmanjs';
 
 const App = () => {
 
-  return (
-    <View>
-      <Text>**Temporary Screen**</Text>
-    </View>
-  );
-};
+    const [longitude, setLongitude] = useState([]);
+    const [latitude, setLatitude] = useState([]);
+
+    const [mapState, setMapState] = useState(false);
+
+    const kflat = new KalmanFilter();
+    const kflong = new KalmanFilter();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (mapState === true) {
+                GetLocation.getCurrentPosition({
+                    enableHighAccuracy: true,
+                    timeout: 15000,
+                })
+                .then(location => {
+                    
+                    console.log(JSON.stringify(kflat.filter(location.latitude)) + ',' + JSON.stringify(kflong.filter(location.longitude)));
+                    setLatitude(latitude => [JSON.stringify(location.latitude), ...latitude]);
+                    setLongitude(longitude => [JSON.stringify(location.longitude), ...longitude]);
+                })
+                .catch(error => {
+                    console.log("Got an error : " + error.message);
+                });
+            }
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [mapState])
+
+    const _getLocation = () => {
+        setMapState(true);
+    }
+
+    const _stopLocation = () => {
+        setMapState(false);
+
+        console.log("Latitude: ")
+        latitude.forEach(function (item, index) {
+            console.log(item);
+        });
+
+        console.log("Longitude: ");
+
+        longitude.forEach(function (item, index) {
+            console.log(item);
+        });
+    }
+
+    const _clearLocation = () => {
+        setLatitude([]);
+        setLongitude([]);
+    }
+
+    return (
+        <View>
+            <View style = {{alignItems:"center", justifyContent:"center"}}>
+                <View style={{display:'flex', flexDirection:'row', marginBottom: 50}}>
+                    <Button
+                        title="Get Location"
+                        onPress={_getLocation}
+                        style={{backgroundColor:'#99004d', margin:200}}>
+                    </Button>
+                    <Button
+                        title="Stop Location"
+                        onPress={_stopLocation}
+                        style={{backgroundColor:'#99004d', margin:200}}>
+                    </Button>
+                    <Button
+                        title="Clear Location"
+                        onPress={_clearLocation}
+                        style={{backgroundColor:'#99004d', margin:200}}>
+                    </Button>
+                </View>
+            </View>
+            <ScrollView>
+                <View style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
+                    <View style={{display:'flex', flexDirection:'column'}}>
+                        {latitude.slice(0, 50).map((l, id) => 
+                            <Text key={id} style={{marginBottom: 20}}>Latitude: {l}</Text>
+                        )}
+                    </View>
+                    <View style={{display:'flex', flexDirection:'column'}}>
+                        {longitude.slice(0, 50).map((l, id) => 
+                            <Text key={id} style={{marginBottom: 20}}>  Longitude: {l}</Text>
+                        )}
+                    </View>
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
 
 export default App;
