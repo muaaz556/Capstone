e81056986ff90f9fc54ebea9664286dadf501131
Salmaan Khan <salmaan_khan_35@hotmail.com>
2022-10-26 01:04:55 -0400

    GPS Test screen added, GPS values displayed

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
 
 import LoginScreen from "./src/screens/LoginScreen";
 import AdminHomeScreen from './src/screens/AdminHomeScreen';
+import GPSTestScreen from './src/screens/GPSTestScreen';
 
 import SplashScreen from  "react-native-splash-screen";
 
@@ -30,6 +31,8 @@ const App = () => {
           <Stack.Screen name="Login" component={LoginScreen} />
 
           <Stack.Screen name="AdminHomeScreen" component={AdminHomeScreen} />
+
+          <Stack.Screen name="GPSTestScreen" component={GPSTestScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
