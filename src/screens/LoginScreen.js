228e7f95f0fb9641e3adf61fcab41960231fe734
Girujan Naguleswaran <ngirujan@gmail.com>
2022-12-28 19:20:55 -0500

    Adding small changes

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { 
     View, 
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 
 import { Box, Button, Center, FormControl, Image, Input, Stack, Text, WarningOutlineIcon } from "native-base";
+import axios from 'axios';
 
 const styles = StyleSheet.create({
     view: {
@@ -33,9 +34,76 @@ const LoginScreen = ({ navigation }) => {
 
     }
 
-    const handleGuestStudentButtonClick = () => {
+    // const handleGuestStudentButtonClick = () => {
+    //     const requestOptions = {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/json" },
+    //         body: JSON.stringify({'x': 1, 'y': 2}),
+    //     };
 
+    //     fetch("http://127.0.0.1:8000/api/insert/", requestOptions);
+    // }
+
+    const [allUser, setAllUsers] = useState([]);
+
+    // const handleGuestStudentButtonClick = async () => {
+    //     try{
+    //         console.log("Here")
+    //         const users = await fetch('http://192.168.0.244:8000/api/gps/').then(response => response.json());
+    //         console.log('Users', users)
+    //     }
+    //     catch (error){ console.log(error)}
+    // }
+
+    const postRequest = () => {
+        fetch('https://a487-38-13-189-73.ngrok.io/api/post-gps', {
+            method: 'POST',
+            headers: {
+                "access-control-allow-origin" : "*",
+                'Accept': 'application/json',
+                'Content-type':'application/json'
+            },
+            body: JSON.stringify({
+                'gps': [
+                    {
+                        'name': 'names',
+                        'gpsCord': {
+                            'x': 10,
+                            'y': 10,
+                        }
+                    }
+                ]
+            })
+        })
     }
+
+    const handleGuestStudentButtonClick = () => {
+        console.log('Fetching data')
+        fetch('https://a487-38-13-189-73.ngrok.io/api/get-gps', {
+            method: 'GET',
+            headers: {
+                "access-control-allow-origin" : "*",
+                'Accept': 'application/json',
+                'Content-type':'application/json'
+            },
+        }).then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw res.json()
+            }
+        }).then(json => {
+            console.log("Good JSON")
+            console.log(json)
+        }).catch(error => {
+            console.log("Bad JSON")
+            console.log(error)
+        })
+    }
+
+    // useEffect(() => {
+    //     handleGuestStudentButtonClick()
+    // }, [])
 
     return (
         <View style={styles.view} >
@@ -60,7 +128,8 @@ const LoginScreen = ({ navigation }) => {
                         </FormControl.ErrorMessage>
                         <Button style={styles.loginButton}>Login</Button>
                         <Button style={{marginTop: 10}} onPress={() => navigation.navigate('AdminHomeScreen')}>Guest Admin</Button>
-                        <Button style={{marginTop: 10}}>Guest Student</Button>
+                        <Button style={{marginTop: 10}} onPress={handleGuestStudentButtonClick}>Guest Student</Button>
+                        <Button style={{marginTop: 10}} onPress={postRequest}>Post Request</Button>
                     </Stack>
                 </FormControl>
             </Box>
